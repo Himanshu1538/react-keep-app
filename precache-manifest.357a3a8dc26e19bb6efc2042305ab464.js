318b68482ac0096239f33606f7f31222cea35ad9
Himanshu1538 <himanshu.chouhan700@gmail.com>
2021-12-09 19:43:14 +0530

    Updates

---
+++ precache-manifest.357a3a8dc26e19bb6efc2042305ab464.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb8333314ed44e080459d831dcdb34f",
+    "url": "/react-keep-app/index.html"
+  },
+  {
+    "revision": "20b8dbced68d3c2242ee",
+    "url": "/react-keep-app/static/js/2.c87bf882.chunk.js"
+  },
+  {
+    "revision": "2b3393c2ba2697069fda",
+    "url": "/react-keep-app/static/js/main.28b0232e.chunk.js"
+  },
+  {
+    "revision": "d8341ccab1138ae88a66",
+    "url": "/react-keep-app/static/js/runtime-main.fd83922d.js"
+  }
+]);
